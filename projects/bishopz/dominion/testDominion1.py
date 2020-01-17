4af22029a99ca509eb7977b8944318116812e6b9
Zachary Bishop <zach.bishop116@gmail.com>
2020-01-16 20:23:54 -0800

    Move code into testUtility

--- projects/bishopz/dominion/testDominion1.py
+++ projects/bishopz/dominion/testDominion1.py
@@ -7,45 +7,18 @@ Created on Tue Oct 13 15:42:42 2015
 
 import Dominion
 import random
+import testUtility
 from collections import defaultdict
 
-#Get player names
+# Get player names
 player_names = ["Annie","*Ben","*Carla"]
 
-#number of curses and victory cards
-if len(player_names)>2:
-    nV=12
-else:
-    nV=8
-nC = -10 + 10 * len(player_names)
+# Set number of Victory and Curse cards
+(nV, nC) = testUtility.setVictoryCurse(len(player_names))
 
-#Define box
-box = {}
-box["Woodcutter"]=[Dominion.Woodcutter()]*10
-box["Smithy"]=[Dominion.Smithy()]*10
-box["Laboratory"]=[Dominion.Laboratory()]*10
-box["Village"]=[Dominion.Village()]*10
-box["Festival"]=[Dominion.Festival()]*10
-box["Market"]=[Dominion.Market()]*10
-box["Chancellor"]=[Dominion.Chancellor()]*10
-box["Workshop"]=[Dominion.Workshop()]*10
-box["Moneylender"]=[Dominion.Moneylender()]*10
-box["Chapel"]=[Dominion.Chapel()]*10
-box["Cellar"]=[Dominion.Cellar()]*10
-box["Remodel"]=[Dominion.Remodel()]*10
-box["Adventurer"]=[Dominion.Adventurer()]*10
-box["Feast"]=[Dominion.Feast()]*10
-box["Mine"]=[Dominion.Mine()]*10
-box["Library"]=[Dominion.Library()]*10
-box["Gardens"]=[Dominion.Gardens()]*nV
-box["Moat"]=[Dominion.Moat()]*10
-box["Council Room"]=[Dominion.Council_Room()]*10
-box["Witch"]=[Dominion.Witch()]*10
-box["Bureaucrat"]=[Dominion.Bureaucrat()]*10
-box["Militia"]=[Dominion.Militia()]*10
-box["Spy"]=[Dominion.Spy()]*10
-box["Thief"]=[Dominion.Thief()]*10
-box["Throne Room"]=[Dominion.Throne_Room()]*10
+# Initialize the box
+box = testUtility.getBoxes(nV)
+
 
 supply_order = {0:['Curse','Copper'],2:['Estate','Cellar','Chapel','Moat'],
                 3:['Silver','Chancellor','Village','Woodcutter','Workshop'],
@@ -53,11 +26,8 @@ supply_order = {0:['Curse','Copper'],2:['Estate','Cellar','Chapel','Moat'],
                 5:['Duchy','Market','Council Room','Festival','Laboratory','Library','Mine','Witch'],
                 6:['Gold','Adventurer'],8:['Province']}
 
-#Pick 10 cards from box to be in the supply.
-boxlist = [k for k in box]
-random.shuffle(boxlist)
-random10 = boxlist[:10]
-supply = defaultdict(list,[(k,box[k]) for k in random10])
+# Define what cards will be in the supply for this game
+supply = testUtility.getSupply(box)
 
 
 #The supply always has these cards
@@ -86,13 +56,16 @@ for name in player_names:
 turn  = 0
 while not Dominion.gameover(supply):
     turn += 1    
-    print("\r")    
+    print("\r")
+    # Print the supply
     for value in supply_order:
         print (value)
         for stack in supply_order[value]:
             if stack in supply:
                 print (stack, len(supply[stack]))
     print("\r")
+
+
     for player in players:
         print (player.name,player.calcpoints())
     print ("\rStart of turn " + str(turn))    
